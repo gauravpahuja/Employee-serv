6fac6745f6bc32c15dec407267f51f7c4b543f80
ashok gupta <32299588+ashoka25395@users.noreply.github.com>
2021-02-17 12:47:56 -0800

    Update Employee.java
    issue resolved

--- employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/models/Employee.java
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/models/Employee.java
@@ -12,12 +12,17 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Column;
+
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "EMPLOYEE")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -28,16 +33,22 @@ import lombok.NoArgsConstructor;
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	@Column(name = "EMPLOYEE_ID")
 	private Integer id;
 
+	@Column(name = "first_name")
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
+	@Column(name = "date_of_birth")
 	private Date dateOfBirth;
 
 	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "ADDRESS_ID", referencedColumnName = "ADDRESS_ID", nullable = false)
 	private Address address;
 
 }
